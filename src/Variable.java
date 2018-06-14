0da646a240bfff6ff87264f3e60612f6b05ccde6
ניצן לוכסמבורג <nitzanlux@gmail.com>
2018-06-14 17:27:35 +0300

    ddd

--- src/Variable.java
+++ src/Variable.java
@@ -1,5 +1,19 @@
 public class Variable {
+    private String value;
+    private String variableName;
+    private Type variableType;
+    public Variable(String variableName,String value,String type){
+        this.value=value;
 
+    }
+    private Type getVariableTypeFromString(String type){
+        for (Type currentType:Type.values()) {
+            if (type.equals(currentType.toString())){
+                return currentType;
+            }
+        }
+        return null;
+    }
     private enum Type{INTEGER, DOUBLE, STRING, CHAR, BOOLEAN;
         private
 
